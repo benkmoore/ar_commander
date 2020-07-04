65bee25d06ec1ca68bb303fb8690cc03519eb500
zach <zhoda93@gmail.com>
2020-07-04 15:22:32 -0700

    Prepare to separate controller class from control functions
    Note, we are not stable here.  This is a preparation for larger architectural changes
    Messy commit

--- scripts/controller.py
+++ scripts/controller.py
@@ -15,75 +15,28 @@ N = 4                           # number of wheels
 R1 = np.array([0.075, 0.425])   # position of wheels along arm 1
 R2 = np.array([0.075, 0.425])   # "" arm 2
 
-class Controller():
-    def __init__(self):
-        rospy.init_node('controller', anonymous=True)
-
-        # current state
-        self.pos = None
-        self.theta = None
-        self.vel = None
-
-        # controller commands (outputs)
-        self.phi_cmd = None
-        self.V_cmd = None
-        self.theta_dot_cmd = 0
+class controlFunction():
+    def __init__():
         self.theta_error_sum = 0
-        self.theta_delta_prev = 0
-        self.p_delta_prev = 0
-
-        # navigation info (inputs)
-        self.pos_des = None
-        self.trajectory = None
-        self.traj_idx = 0
-
-        # subscribers
-        rospy.Subscriber('/pose', Pose2D, self.poseCallback)
-        rospy.Subscriber('/cmd_waypoint', Pose2D, self.waypointCallback)
-        rospy.Subscriber('/cmd_trajectory', Trajectory, self.trajectoryCallback)
-
-        # publishers
-        self.pub_cmds = rospy.Publisher('/controller_cmds', ControllerCmd, queue_size=10)
-
-    ## Callback Functions
-    def waypointCallback(self, msg):
-        self.pos_des = Vector3()
-        self.pos_des.x = msg.x
-        self.pos_des.y = msg.y
-        self.theta_des = msg.theta
-
-    def trajectoryCallback(self, msg):
-        if self.trajectory is None:
-            self.trajectory = np.concatenate((np.array(msg.x.data).reshape(-1,1), \
-                                              np.array(msg.y.data).reshape(-1,1), \
-                                              np.array(msg.theta.data).reshape(-1,1)), axis=1)
-
-    def poseCallback(self, msg):
-        if self.pos is None:
-            self.pos = np.zeros(2)
-        self.pos[0] = msg.x
-        self.pos[1] = msg.y
-        self.theta = msg.theta
 
     ## Controller Functions
-    def pointController(self):
+    def pointController(self, p_des, th_des):
         kp_1 = 10
         kd_1 = 0
         kp_2 = 3
         ki_2 = 0.03
         kd_2 = 5e-8
 
-        p_des = np.array([self.pos_des.x,self.pos_des.y])
         p_delta = p_des-self.pos
-        theta_delta = self.theta_des-self.theta
+        theta_delta = theta_des-self.theta
         self.theta_error_sum += theta_delta
 
         if npl.norm(p_delta) > 0.01:
-            v_cmd = kp_1*p_delta + kd_1*(p_delta-self.p_delta_prev)
+            v_cmd = kp_1*p_delta + kd_1*(p_delta - p_delta_prev)
         else:
             v_cmd = np.array([1e-15,1e-15])
         theta_dot_cmd = kp_2*theta_delta + ki_2*self.theta_error_sum \
-                        + kd_2*(theta_delta-self.theta_delta_prev)
+                        + kd_2*(theta_delta - self.theta_delta_prev)
 
         self.theta_delta_prev = theta_delta
         self.p_delta_prev = p_delta
@@ -113,11 +66,7 @@ class Controller():
                 wp_prev = wp
                 wp = self.trajectory[self.traj_idx, 0:2]
             else:
-                self.pos_des = Vector3()
-                self.pos_des.x = self.pt_next[0]
-                self.pos_des.y = self.pt_next[1]
-                self.theta_des = 0
-                return self.pointController() #np.zeros(N), 0
+                return self.pointController()
 
         # fit line/poly and get derivative
         x = np.array([wp_prev[0], wp[0]])
@@ -143,6 +92,47 @@ class Controller():
 
         return V_cmd, phi_cmd, v_cmd
 
+
+class Controller():
+    def __init__(self):
+        rospy.init_node('controller', anonymous=True)
+
+        # current state
+        self.pos = None
+        self.theta = None
+        self.vel = None
+        self.omega = None
+
+        # controller commands (outputs)
+        self.phi_cmd = None
+        self.V_cmd = None
+
+        # navigation info (inputs)
+        self.trajectory = None
+        self.traj_idx = 0
+
+        # subscribers
+        rospy.Subscriber('/pose', Pose2D, self.poseCallback)
+        rospy.Subscriber('/cmd_trajectory', Trajectory, self.trajectoryCallback)
+
+        # publishers
+        self.pub_cmds = rospy.Publisher('/controller_cmds', ControllerCmd, queue_size=10)
+
+    ## Callback Functions
+    def trajectoryCallback(self, msg):
+        if self.trajectory is None:
+            self.trajectory = np.concatenate((np.array(msg.x.data).reshape(-1,1), \
+                                              np.array(msg.y.data).reshape(-1,1), \
+                                              np.array(msg.theta.data).reshape(-1,1)), axis=1)
+
+    def poseCallback(self, msg):
+        if self.pos is None:
+            self.pos = np.zeros(N)
+        self.pos[0] = msg.x
+        self.pos[1] = msg.y
+        self.theta = msg.theta
+
+    ## Helper Function
     def convert2motorInputs(self, v_cmd_gf, theta_dot_cmd):
         # Arm frame = af, Robot frame = rf, Global frame = gf
         v_th_af = np.concatenate((np.zeros((1,N)), (np.concatenate((R1*theta_dot_cmd, R2*theta_dot_cmd))).reshape(1,-1)))
@@ -164,11 +154,7 @@ class Controller():
         self.V_cmd = np.zeros(N)
         self.phi_cmd = np.zeros(N) # rads
 
-        if self.pos is not None:
-            # Point controller
-            if self.pos_des is not None:
-                self.V_cmd, self.phi_cmd, self.vel = self.pointController()
-            # Trajectory controller
+        if self.pos is not None:    # TODO: Replace this with a mode check
             if self.trajectory is not None:
                 self.V_cmd, self.phi_cmd, self.vel = self.trajectoryController()
 
