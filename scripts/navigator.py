dac7a4a03d423398ff8ca3f920d294cc59b9fedf
zach <zhoda93@gmail.com>
2020-07-12 15:10:11 -0700

    Modify navigator to use fsm mode

--- scripts/navigator.py
+++ scripts/navigator.py
@@ -4,16 +4,17 @@ import rospy
 import numpy.linalg as npl
 
 from astar import *
-from ar_commander.msg import Trajectory
-from std_msgs.msg import Float64MultiArray
-from geometry_msgs.msg import Pose2D
+from stateMachine import Mode
+
+from ar_commander.msg import Trajectory, State
+from std_msgs.msg import Int8
 
 
 class Navigator():
     def __init__(self):
         rospy.init_node('navigator')
 
-        self.trajectory_complete = False
+        self.trajectory_published = False
         self.map_width = 10
         self.map_height = 10
         self.obstacles = [((6,6),(8,7)),((2,0),(8,2)),((2,4),(4,6)),((6,2),(8,4))]
@@ -21,20 +22,28 @@ class Navigator():
         self.start_pos = (None, None)
         self.end_pos = (8.0,8.0)
         self.pos = np.array([None, None])
+        self.theta = None
+        self.mode = None
+
         self.trajectory = None
+
+
+        # subscribers
+        rospy.Subscriber('estimator/state', State, self.stateCallback)
+        rospy.Subscriber('state_machine/mode', Int8, self.modeCallback)
 
         # publishers
         self.pub_trajectory = rospy.Publisher('/cmd_trajectory', Trajectory, queue_size=10)
 
-        # subscribers
-        rospy.Subscriber('/pose', Pose2D, self.poseCallback)
 
-    def poseCallback(self, msg):
-        self.pos[0] = msg.x
-        self.pos[1] = msg.y
-        self.theta = msg.theta
+    def stateCallback(self, msg):
+        self.pos = np.array(msg.pos.data)
+        self.theta = msg.theta.data
         if self.start_pos[0] == None:
             self.start_pos = (round(self.pos[0],1), round(self.pos[1],1))
+
+    def modeCallback(self,msg):
+        self.mode = Mode(msg.data)
 
     def callAstar(self):
         occupancy = DetOccupancyGrid2D(self.map_width, self.map_height, self.obstacles)
@@ -49,21 +58,16 @@ class Navigator():
         self.trajectory.theta.data = np.zeros((len(self.trajectory.x.data),1))
         #astar.plot_path()
 
-    def testTrajectoryComplete(self):
-        if npl.norm(self.end_pos-self.pos) < 0.1:
-            self.trajectory_complete = True
-
     def publish(self):
         self.pub_trajectory.publish(self.trajectory)
+        self.trajectory_published = True
 
     def run(self):
         rate = rospy.Rate(10) # 10 Hz
-        while not rospy.is_shutdown() and not self.trajectory_complete:
-            if self.trajectory == None and self.start_pos[0] != None:
+        while not rospy.is_shutdown() and not self.trajectory_published:
+            if self.mode == Mode.IDLE:
                 self.callAstar()
-            if self.trajectory != None:
                 self.publish()
-                self.testTrajectoryComplete()
             rate.sleep()
 
 if __name__ == '__main__':
