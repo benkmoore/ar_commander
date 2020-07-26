430e9e4a86c1f9dc49dfcb960b0606ebeb1a2911
zach <zhoda93@gmail.com>
2020-07-26 14:51:01 -0700

    Add robot commands to controller output message
    This will be useful for other nodes to know what the commanded robot velociy and omega are
    Needed for open loop estimator
    Due to a bug in catkin, run `touch CMakeLists.txt` before `catkin_make`

--- scripts/controller.py
+++ scripts/controller.py
@@ -110,8 +110,10 @@ class ControlNode():
         self.controllers = ControlLoops()
 
         # output commands
-        self.phi_cmd = None
-        self.V_cmd = None
+        self.wheel_phi_cmd = None
+        self.wheel_v_cmd = None
+        self.robot_v_cmd = None
+        self.robot_omega_cmd = None
 
         # subscribers
         rospy.Subscriber('estimator/state', State, self.stateCallback)
@@ -135,7 +137,7 @@ class ControlNode():
     def modeCallback(self,msg):
         self.mode = Mode(msg.data)
 
-    ## Helper Functions        
+    ## Helper Functions
     def getWaypoint(self):
         # determine waypoint
         wp = self.trajectory[self.traj_idx, :]
@@ -174,8 +176,10 @@ class ControlNode():
     ## Main Loops
     def controlLoop(self):
         # default behavior
-        self.V_cmd = np.zeros(rcfg.N)
-        self.phi_cmd = np.zeros(rcfg.N) # rads
+        self.wheel_v_cmd = np.zeros(rcfg.N)
+        self.wheel_phi_cmd = np.zeros(rcfg.N) # rads
+        self.robot_v_cmd = np.zeros(2)
+        self.robot_omega_cmd = 0
 
         if self.mode == Mode.TRAJECTORY:
             wp, wp_prev = self.getWaypoint()
@@ -185,14 +189,19 @@ class ControlNode():
                 v_des = self.controllers.pointController(wp[0:2], self.pos, self.vel)
                 w_des = self.controllers.thetaController(wp[2], self.theta, self.omega)
 
-            self.V_cmd, self.phi_cmd = self.convert2MotorInputs(v_des,w_des)
+            self.wheel_v_cmd, self.wheel_phi_cmd = self.convert2MotorInputs(v_des,w_des)
+            self.robot_v_cmd = v_des
+            self.robot_omega_cmd = w_des
 
     def publish(self):
         """ publish cmd messages """
-        self.cmds = ControllerCmd()
-        self.cmds.velocity_arr.data = self.V_cmd
-        self.cmds.phi_arr.data = self.phi_cmd
-        self.pub_cmds.publish(self.cmds)
+        cmd = ControllerCmd()
+        cmd.velocity_arr.data = self.wheel_v_cmd
+        cmd.phi_arr.data = self.wheel_phi_cmd
+        cmd.robot_vel.data = self.robot_v_cmd
+        cmd.robot_omega.data = self.robot_omega_cmd
+
+        self.pub_cmds.publish(cmd)
 
     def run(self):
         rate = rospy.Rate(params.CONTROLLER_RATE)
