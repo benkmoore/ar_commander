ecc87574e9e300f5bfe6375ee49a340bf434b2ba
benkmoore <ben.karl.moore@gmail.com>
2020-08-02 10:14:13 +0100

    added wheel radius to estimator OL. tested versus ground truth and error is ~0.01 on x, y and theta

--- configs/robot_v1.py
+++ configs/robot_v1.py
@@ -4,3 +4,4 @@ import numpy as np
 N = 4                           # number of wheels
 R1 = np.array([0.075, 0.425])   # position of wheels along arm 1
 R2 = np.array([0.075, 0.425])   # "" arm 2
+WHEEL_RADIUS = 0.05             # meters
