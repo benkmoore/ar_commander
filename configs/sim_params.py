63b0650c0027887c57d6586f975094d87cc6d1ec
benkmoore <ben.karl.moore@gmail.com>
2020-08-12 13:37:20 +0100

    adjusted params to slow down robot to compare true vel and commanded vel

--- configs/sim_params.py
+++ configs/sim_params.py
@@ -3,6 +3,6 @@ CONTROLLER_RATE = 10                       # rate in Hz
 
 thetaControllerGains = {'kp':3, 'ki':0.03, 'kd':5e-8}
 
-pointControllerGains = {'kp':10, 'kd':0}
+pointControllerGains = {'kp':0.5, 'kd':0}
 
-trajectoryControllerGains = {'kp_pos':12, 'kp_th':0.75, 'kd_pos':0.5, 'v_mag':6}
+trajectoryControllerGains = {'kp_pos':12, 'kp_th':0.75, 'kd_pos':0.5, 'v_mag':0.5}
