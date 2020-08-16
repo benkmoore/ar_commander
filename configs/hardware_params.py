0711c7f1be90b8098870d383f4b3fcb26069e51c
benkmoore <ben.karl.moore@gmail.com>
2020-08-16 09:48:42 +0100

    updated wp param comment

--- configs/hardware_params.py
+++ configs/hardware_params.py
@@ -3,7 +3,7 @@ CONTROLLER_RATE = 70                       # rate in Hz
 
 max_vel = 3                                # m/s
 
-wp_threshold = 0.05                        # m, waypoint threshold
+wp_threshold = 0.05                        # distance threshold at which we transition to next waypoint (m)
 
 thetaControllerGains = {'kp':175, 'ki':0.03, 'kd':5e-8}
 
