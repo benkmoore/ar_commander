dc660ff43202e0f0d5c233e44b9eccfd93e60c1c
benkmoore <ben.karl.moore@gmail.com>
2020-08-17 15:22:07 +0100

    added KF for decawave localiation. Tested with noisy data in sim

--- configs/sim_params.py
+++ configs/sim_params.py
@@ -9,4 +9,4 @@ thetaControllerGains = {'kp':3, 'ki':0.03, 'kd':5e-2}
 
 pointControllerGains = {'kp':0.5, 'kd':0}
 
-trajectoryControllerGains = {'kp_pos':12, 'kp_th':0.75, 'kd_pos':0.5, 'k_ol':5}
+trajectoryControllerGains = {'kp_pos':0.25, 'kp_th':0.75, 'kd_pos':0.5, 'k_ol':0.5}
