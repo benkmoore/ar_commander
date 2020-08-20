0022d8e7f2df47c0a01929099d32144f71ae2d0c
kevin oflannagain <kevinoflannagain@gmail.com>
2020-08-20 21:16:52 +0100

    removed while loop from function, changed some names, moved some code from functions to init

--- scripts/localizer.py
+++ scripts/localizer.py
@@ -2,6 +2,7 @@
 import serial
 import time
 import re
+import sys
 import rospy
 import numpy as np
 from geometry_msgs.msg import Pose2D
@@ -14,18 +15,7 @@ class Localizer():
     def __init__(self):
         rospy.init_node('localization')
 
-        self.pose2D = Pose2D()
-        self.pose2D.x = None
-        self.pose2D.y = None
-        self.pose2D.theta = None
-        #confidence not used right now but i think we should in future
-        self.confidence = None
-        self.failConnect = 0
-        # publishers
-        self.pub_localize = rospy.Publisher('sensor/decawave_measurement', Pose2D, queue_size=10)
-
-
-    def startup(self):
+        # pySerial serial stuff
         self.ser = serial.Serial()
         self.ser.port = '/dev/ttyACM1'
         self.ser.baudrate = 115200
@@ -33,10 +23,29 @@ class Localizer():
         self.ser.parity =serial.PARITY_NONE
         self.ser.stopbits = serial.STOPBITS_ONE
         self.ser.timeout = 0.3
-        self.ser.open()
+
+        #Pose stuff, theta not used yet
+        self.pose2D = Pose2D()
+        self.pose2D.x = None
+        self.pose2D.y = None
+        self.pose2D.theta = None
+
+        #confidence not published right now but we will soon
+        #its the value that the boards give for how confident they are
+        self.confidence = None
+        self.readFails = 0
+
+        # publishers
+        self.pub_localize = rospy.Publisher('sensor/decawave_measurement', Pose2D, queue_size=10)
+
+
+    def connect(self):        
+        if not self.ser.is_open:
+            self.ser.open()
         self.ser.write(b'\r\r')
+        #this sleep is important until we find a better way
         time.sleep(1)
-        print "startup"
+        rospy.loginfo("Decawave serial connecting")
 
         #here we reset the input buffer to 0, then wait to see if it fills again
         #if the buffer fills, this means that the board is already sending info down the serial line
@@ -48,42 +57,36 @@ class Localizer():
 
 
     #check the data from the board using pyserial
-    def checkSerial(self):
-        fails = 0
-        #this while should maybe be something else, we have while not rospy.is_shutdown(): in run() as well
-        while not rospy.is_shutdown():
-            try:
-                
-                data = str(self.ser.readline())
-                data = re.sub("[a-zA-Z]", "", data)
-                data = re.sub("[\r\n>]","",data)
-                #print data
-                #3 is still the length even when we remove all the characters??
-                if len(data) > 3:
-                    data = np.array(data.split(','))
-                    data = data[1:4]
-                    data = data.astype(np.float)
-                    print data
-                    self.pose2D.x = data[0]
-                    self.pose2D.y = data[1]
-                    self.pose2D.theta = THETA
-                    self.publish()
-                    print(data)
-                    fails = 0
-                else:
-                    #self.ser.write(b'lep\r')
-                    fails += 1
-                if fails > 10:
-                    print ('Localizer missed {} packages, check connection between boards + orientation of tag.').format(fails)
-                    
+    def readSerial(self):        
+        try:            
+            data = str(self.ser.readline())
+            data = re.sub("[a-zA-Z]", "", data)
+            data = re.sub("[\r\n>]","",data)
+            # print data
+            #3 is still the length even when we remove all the characters??
+            if len(data) > 3:
+                data = np.array(data.split(','))
+                data = data[1:5]
+                data = data.astype(np.float)
+                print data
+                self.pose2D.x = data[0]
+                self.pose2D.y = data[1]
+                self.pose2D.theta = THETA
+                self.confidence = data[3]
+                self.publish()
+                self.readFails = 0
+            else:
+                self.readFails += 1
 
-            except Exception as e:
-                print(e)
-                # self.ser.write(b'lep\r')
-                # self.ser.close()
-                #self.startup()
-               
-                pass
+                #if we dont read any pose data for this amount of tries then redo the serial connection
+            if self.readFails > 30:
+                #print ('Localizer missed {} packages, check connection between boards + orientation of tag.').format(self.readFails)
+                self.readFails = 0
+                self.connect()
+
+        except Exception as e:
+            print(e)         
+            pass
 
 
             
@@ -99,14 +102,14 @@ class Localizer():
 
 
     def run(self):
-        self.startup()
+        self.connect()
         rate = rospy.Rate(RATE) # 10 Hz
         while not rospy.is_shutdown():
-            self.checkSerial()
-            # self.closeConnection()     
-            rate.sleep()
-        #rospy.spin()
+            self.readSerial()
 
+            # rate.sleep()
+        self.closeConnection()    
+        #rospy.spin()
 
 
 if __name__ == '__main__':
