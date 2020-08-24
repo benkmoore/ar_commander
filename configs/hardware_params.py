0b85665d6084c2dff3eefa1e18e5d521139d0afc
benkmoore <ben.karl.moore@gmail.com>
2020-08-24 23:22:33 +0100

    fixed dims in hardware params

--- configs/hardware_params.py
+++ configs/hardware_params.py
@@ -13,6 +13,6 @@ pointControllerGains = {'kp':180, 'kd':0}
 trajectoryControllerGains = {'kp_pos':40, 'kp_th':0.75, 'kd_pos':0.5, 'k_ol':50}
 
 # noise/uncertainty estimate on predict (Q) meas. (R) process and derivatives (d)
-localizationFilterParams = {'Q':10*np.eye(3), 'Q_d':100*np.eye(3), 'R':0.01*np.eye(3), 'R_d':np.eye(3)}
+localizationFilterParams = {'Q':10*np.eye(2), 'Q_d':100*np.eye(2), 'R':0.01*np.eye(2), 'R_d':np.eye(2)}
 
 thetaFilterParams = {'Q':10, 'Q_d':100, 'R':0.01, 'R_d':100}
