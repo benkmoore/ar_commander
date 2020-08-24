a08e5fcec93762b35c540c5916f075dc0464b94c
benkmoore <ben.karl.moore@gmail.com>
2020-08-24 17:42:24 +0100

    remove gain changes

--- configs/sim_params.py
+++ configs/sim_params.py
@@ -3,10 +3,10 @@ CONTROLLER_RATE = 10                       # rate in Hz
 
 max_vel = 3                                # m/s
 
-wp_threshold = 0.25                        # distance threshold at which we transition to next waypoint (m)
+wp_threshold = 0.05                        # distance threshold at which we transition to next waypoint (m)
 
 thetaControllerGains = {'kp':3, 'ki':0.03, 'kd':5e-2}
 
 pointControllerGains = {'kp':0.5, 'kd':0}
 
-trajectoryControllerGains = {'kp_pos':1, 'kp_th':1, 'kd_pos':0.5, 'k_ol':0.25}
+trajectoryControllerGains = {'kp_pos':12, 'kp_th':0.75, 'kd_pos':0.5, 'k_ol':5}
