c1f378facfd82a5254772bddebd97b01979a69eb
benkmoore <ben.karl.moore@gmail.com>
2020-08-24 23:19:37 +0100

    restructured KF into seperate pos and theta filters. Tested in sim, looks good

--- scripts/estimator.py
+++ scripts/estimator.py
@@ -34,28 +34,49 @@ class Estimator():
         self.pos_meas = None
         self.theta_meas = None
 
+        # controller cmds
+        self.vel_cmd = None
+        self.omega_cmd = None
+
         # robot state (output)
         self.state = None
 
-        # init localization KF
-        self.x = None #state
-        self.sigma = None #covariance
-        A = np.eye(6)
-        B = np.eye(6)
-        C = np.eye(6)
-        Q = np.block([[params.localizationFilterParams['Q'], np.zeros((3,3))], [np.zeros((3,3)), params.localizationFilterParams['Q_d']]])
-        R = np.block([[params.localizationFilterParams['R'], np.zeros((3,3))], [np.zeros((3,3)), params.localizationFilterParams['R_d']]])
-        self.localization_filter = LocalizationFilter(dt=self.dt, x0=None, sigma0=10*np.eye(6), A=A, B=B, C=C, Q=Q, R=R)
+        # init KF for postion
+        self.pos_state = None #state
+        self.pos_cov = None #covariance
+        A_pos = np.eye(4)
+        B_pos = np.eye(4)
+        C_pos = np.eye(4)
+        D_pos = np.block([[np.zeros((2,2)), np.zeros((2,2))], [np.eye(2), np.zeros((2,2))]])
+        Q_pos = np.block([[params.positionFilterParams['Q'], np.zeros((2,2))], [np.zeros((2,2)), params.positionFilterParams['Q_d']]])
+        R_pos = np.block([[params.positionFilterParams['R'], np.zeros((2,2))], [np.zeros((2,2)), params.positionFilterParams['R_d']]])
+        self.pos_filter = LocalizationFilter(dt=self.dt, x0=None, sigma0=10*np.eye(4), A=A_pos, B=B_pos, C=C_pos, D=D_pos, Q=Q_pos, R=R_pos)
+
+        # init KF for theta
+        self.theta_state = None #state
+        self.theta_cov = None #covariance
+        A_theta = np.eye(2)
+        B_theta = np.eye(2)
+        C_theta = np.eye(2)
+        D_theta = np.array([[0, 0], [1, 0]])
+        Q_theta = np.array([[params.thetaFilterParams['Q'], 0], [0, params.thetaFilterParams['Q_d']]])
+        R_theta = np.array([[params.thetaFilterParams['R'], 0], [0, params.thetaFilterParams['R_d']]])
+        self.theta_filter = LocalizationFilter(dt=self.dt, x0=None, sigma0=10*np.eye(2), A=A_theta, B=B_theta, C=C_theta, D=D_theta, Q=Q_theta, R=R_theta)
 
         # subscribers
         rospy.Subscriber('sensor/decawave_measurement', Pose2D, self.localizeCallback)
+        rospy.Subscriber('/controller_cmds', ControllerCmd, self.controllerCmdCallback)
 
         # publishers
         self.pub_state = rospy.Publisher('estimator/state', State, queue_size=10)
 
+    def controllerCmdCallback(self, msg):
+        self.vel_cmd = np.array([[msg.robot_vel.data[0]], [msg.robot_vel.data[1]]])
+        self.omega_cmd = msg.robot_omega.data
+
     def localizeCallback(self, msg):
-        self.pos_meas = np.array([msg.x,msg.y])
-        self.theta_meas = np.array([msg.theta])
+        self.pos_meas = np.array([[msg.x],[msg.y]])
+        self.theta_meas = msg.theta
 
     def updateState(self):
         """
@@ -74,15 +95,19 @@ class Estimator():
             self.state.vel.data = np.zeros(2)
             self.state.omega.data = 0
         else: # run localization filter
-            d = np.concatenate(((self.pos_meas - self.state.pos.data),(self.theta_meas - self.state.theta.data)))/self.dt #derviative
-            y = np.concatenate((self.pos_meas, self.theta_meas, d)) # measurement
-            u = np.concatenate((self.state.vel.data, np.array([self.state.omega.data]), np.zeros(3))).reshape(-1)
-            self.x, self.sigma = self.localization_filter.step(u, y)
+            u_pos = np.vstack((self.vel_cmd, np.zeros((2,1))))
+            y_pos = np.vstack((self.pos_meas, np.zeros((2,1))))
 
-            self.state.pos.data = self.x[0:2]
-            self.state.theta.data = self.x[2]
-            self.state.vel.data = self.x[3:5]
-            self.state.omega.data = self.x[5]
+            u_theta = np.vstack((self.omega_cmd, 0))
+            y_theta = np.vstack((self.theta_meas, 0))
+
+            self.pos_state, self.pos_cov = self.pos_filter.step(u_pos, y_pos)
+            self.theta_state, self.theta_cov = self.theta_filter.step(u_theta, y_theta)
+
+            self.state.pos.data = self.pos_state[0:2]
+            self.state.vel.data = self.pos_state[2:4]
+            self.state.theta.data = self.theta_state[0]
+            self.state.omega.data = self.theta_state[1]
 
     def publish(self):
         self.pub_state.publish(self.state)
--- scripts/localization_filter.py
+++ scripts/localization_filter.py
@@ -3,7 +3,7 @@ import numpy.linalg as npl
 
 
 class LocalizationFilter():
-    def __init__(self, dt, x0, sigma0, A, B, C, Q, R):
+    def __init__(self, dt, x0, sigma0, A, B, C, D, Q, R):
         self.dt = dt
         self.x = x0
         self.x_pred = None
@@ -12,6 +12,7 @@ class LocalizationFilter():
         self.A = A
         self.B = B
         self.C = C
+        self.D = D
         self.Q = Q
         self.R = R
 
@@ -20,7 +21,7 @@ class LocalizationFilter():
         self.sigma_pred = npl.multi_dot([self.A, self.sigma, self.A.T]) + self.Q
 
     def update(self, y):
-        y_delta = np.concatenate((y[0:2] - self.x_pred[0:2], np.zeros(1), y[3:5] - self.x_pred[3:5], np.zeros(1))) # only have 2d localization, no theta measurement yet
+        y_delta = y - self.x_pred
         innovation_cov = npl.inv(npl.multi_dot([self.C, self.sigma_pred, self.C.T]) + self.R)
 
         self.x = self.x_pred + npl.multi_dot([self.sigma_pred, self.C.T, innovation_cov, y_delta])
