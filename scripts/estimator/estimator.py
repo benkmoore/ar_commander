018f44060167b19f16a2191333a74f1ad120a36c
benkmoore <ben.karl.moore@gmail.com>
2020-09-25 17:04:27 +0100

    typo fix

--- scripts/estimator/estimator.py
+++ scripts/estimator/estimator.py
@@ -65,7 +65,7 @@ class Estimator():
         tf_angle = self.state.theta.data if self.state is not None else self.theta_meas
         self.pos_meas1 = np.array([msg.x1.data+rcfg.L*np.sin(tf_angle),
                                     msg.y1.data-rcfg.L*np.cos(tf_angle)]) # sensor on robot Y axis arm
-        self.pos_meas2 = np.array([msg.x2.data-rcfg.L*np.cos(tf_angle), 
+        self.pos_meas2 = np.array([msg.x2.data-rcfg.L*np.cos(tf_angle),
                                     msg.y2.data-rcfg.L*np.sin(tf_angle)]) # sensor on robot X axis arm
 
         # update measurement flags
