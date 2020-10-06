fixed uncertainty prop calculation and replaced reshape with flatten

scripts/sensorInterface/decawaveInterface.py
@@ -5,6 +5,8 @@ import re
 import sys
 import rospy
 import numpy as np
+import numpy.linalg as npl
+import scipy.linalg as spl
 import collections
 from ar_commander.msg import Decawave
 
@@ -14,7 +16,7 @@ import configs.hardware_params as params
 
 PORT1 = params.decawave_ports[0]  # sensor 1 usb port: Y axis arm of robot
 PORT2 = params.decawave_ports[1]  # sensor 2 usb port: X axis arm of robot
-SERIALTIMEOUT = 0.3     # duration of serial read (s) 
+SERIALTIMEOUT = 0.3     # duration of serial read (s)
 NUM_READ_FAILS = 30     # number of read attempts before serial connection is restarted
 RATE = 10               # (Hz)
 
@@ -44,15 +46,15 @@ class DecaInterface():
         Connect to decawave board and to request localization data
 
         Shell mode is entered via a serial write command and the serial
-        buffer is reset before data is requested. Before writing 'lep' to 
-        the board the input buffer is reset to 0. If the buffer fills, this 
-        means that the board is already sending info down the serial line 
+        buffer is reset before data is requested. Before writing 'lep' to
+        the board the input buffer is reset to 0. If the buffer fills, this
+        means that the board is already sending info down the serial line
         and if we send our command in this case then it will stop the info.
         """
 
         if not self.ser.is_open:
             self.ser.open()
-        
+
         self.ser.write(b'\r\r') # enter shell mode to talk to the board
         time.sleep(1) # comms setup time
         rospy.loginfo("Decawave serial connecting")
@@ -84,7 +86,7 @@ class DecaInterface():
             self.readFails += 1
             self.dataRead = False
             rospy.logerr("on port %s, readSerial failed to read serial data %s times.", self.ser.port, self.readFails)
-        
+
         if self.readFails > NUM_READ_FAILS: # check connection between boards + orientation of tag.
             rospy.logerr("readSerial failed to read serial data %s times. Check: 1. Anchors xy pos is accurate, 2. Enough anchors in range, 3. Orientation of tag.", self.readFails)
             self.readFails = 0
@@ -134,10 +136,10 @@ class GetPose():
     def calculateCovs(self):
         """
         Calculate measurement covariances.
-        
+
         Uses decawave hardware param, standard deviation on position measurement and the outputted confidence in the
-        measurement timestamp from the board. Propagates uncertainty by combining the measurement covariances from 
-        each sensor to find a covariance for the theta measurement with a linear combination. The derivates are 
+        measurement timestamp from the board. Propagates uncertainty by combining the measurement covariances from
+        each sensor to find a covariance for the theta measurement with a linear combination. The derivates are
         derived from the heading calculation, the formula relates robot heading to sensor measurements.
         """
         self.cov_pos1 = (self.pos_meas_std**2)*np.eye(2)
@@ -145,18 +147,9 @@ class GetPose():
 
         dx = self.pos2[0] - self.pos1[0]
         dy = self.pos2[1] - self.pos1[1]
-        div = (dy**2 + dx**2)
+        J = np.abs(np.array([dy, -dx, -dy, dx])) / (dy**2 + dx**2)
 
-        dth_dx1 =  dy / div
-        dth_dy1 = -dx / div
-        dth_dx2 = -dy / div
-        dth_dy2 =  dx / div
-
-        dth_dp1 = np.abs(np.array([dth_dx1, dth_dy1]))
-        dth_dp2 = np.abs(np.array([dth_dx2, dth_dy2]))
-
-        std_theta = np.dot(dth_dp1, np.sqrt(self.cov_pos1)) + np.matmul(dth_dp2, np.sqrt(self.cov_pos2))
-        self.cov_theta = np.mean(std_theta) ** 2
+        self.cov_theta = npl.multi_dot((J, spl.block_diag(self.cov1, self.cov2), J.T))
 
 
     def updateMeasurementMsgData(self):
@@ -168,14 +161,14 @@ class GetPose():
         self.measurement_msg.theta.data = self.theta
 
         # covariances
-        self.measurement_msg.cov1.data = self.cov_pos1.reshape(-1)
-        self.measurement_msg.cov2.data = self.cov_pos2.reshape(-1)
+        self.measurement_msg.cov1.data = self.cov_pos1.flatten()
+        self.measurement_msg.cov2.data = self.cov_pos2.flatten()
         self.measurement_msg.cov_theta.data = self.cov_theta
 
         # flags
         self.measurement_msg.new_meas1.data = self.boardY.dataRead
         self.measurement_msg.new_meas2.data = self.boardX.dataRead
-        
+
 
     def obtainMeasurements(self):
         if self.boardY.dataRead and self.boardX.dataRead:
@@ -191,7 +184,7 @@ class GetPose():
 
 
     def run(self):
-        rate = rospy.Rate(RATE) 
+        rate = rospy.Rate(RATE)
         while not rospy.is_shutdown():
             self.boardY.readSerial()
             self.boardX.readSerial()