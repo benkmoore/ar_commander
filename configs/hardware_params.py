c102e5be9a6115b7936c794cfc2e4cf03c8c4330
benkmoore <ben.karl.moore@gmail.com>
2020-10-25 14:04:21 +0000

    referenced a jira ticket to controller design

--- configs/hardware_params.py
+++ configs/hardware_params.py
@@ -8,7 +8,7 @@ wp_threshold = 0.05                        # distance threshold at which we tran
 
 theta_threshold = np.pi/8				   # robot theta threshold at which we transition to next waypoint (rad)
 
-# controller gains: Kp = 1.0, Ki = 0.01, Kd = 0.25, rate = 70 Hz
+# controller gains: Kp = 1.0, Ki = 0.01, Kd = 0.25, rate = 70 Hz see: AR1-192
 ctrl_tf_state = {'num': np.array([ 0.01418517, -0.01418314]), 'den': np.array([ 1.0 , -1.98581382,  0.98581584])}
 ctrl_tf_state_dot = {'num': np.array([0.00356506]]), 'den': np.array([ 1.0, -0.99643494])}
 
--- configs/sim_params.py
+++ configs/sim_params.py
@@ -8,7 +8,7 @@ wp_threshold = 0.25                        # distance threshold at which we tran
 
 theta_threshold = np.pi/8				   # robot theta threshold at which we transition to next waypoint (rad)
 
-# controller gains: Kp = 1.0, Ki = 0.01, Kd = 0.25, rate = 10 Hz
+# controller gains: Kp = 1.0, Ki = 0.01, Kd = 0.25, rate = 10 Hz see: AR1-192
 ctrl_tf_state = {'num': np.array([ 0.09520937, -0.09511421]), 'den': np.array([ 1.0 , -1.90474226,  0.90483742])}
 ctrl_tf_state_dot = {'num': np.array([0.02469009]), 'den': np.array([ 1.0, -0.97530991])}
 
