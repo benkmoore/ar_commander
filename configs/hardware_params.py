8b61052776e0cc9c49793c1f59672966524265c2
benkmoore <ben.karl.moore@gmail.com>
2020-10-26 10:54:50 +0000

    fixed bracket typo on params

--- configs/hardware_params.py
+++ configs/hardware_params.py
@@ -10,7 +10,7 @@ time_threshold = 1                         # time threshold about final trajecto
 
 # controller gains: Kp = 1.0, Ki = 0.01, Kd = 0.25, rate = 70 Hz see: AR1-192
 ctrl_tf_state = {'num': np.array([ 0.01418517, -0.01418314]), 'den': np.array([ 1.0 , -1.98581382,  0.98581584])}
-ctrl_tf_state_dot = {'num': np.array([0.00356506]]), 'den': np.array([ 1.0, -0.99643494])}
+ctrl_tf_state_dot = {'num': np.array([0.00356506]), 'den': np.array([ 1.0, -0.99643494])}
 
 # filter params obtained from data and testing at: AR1-142
 # Q : covariance on the process noise
