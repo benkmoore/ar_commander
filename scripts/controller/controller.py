6109858fd5b18c59b4177fea7357b09a8dccb2b4
benkmoore <ben.karl.moore@gmail.com>
2020-11-03 12:34:17 +0000

    fixed bug

--- scripts/controller/controller.py
+++ scripts/controller/controller.py
@@ -208,10 +208,10 @@ class ControlNode():
             v_des, w_des = self.trajectoryController.getControlCmds(self.pos, self.theta, self.vel, self.omega)
             try:
                 t = time.time() - self.trajectoryController.init_traj_time
+                if npl.norm(self.trajectoryController.trajectory[-1,0:2]-self.pos) < params.wp_threshold and (t > self.trajectoryController.t[-1] or abs(t - self.trajectoryController.t[-1]) < params.time_threshold): # check if near end pos and end time
+                    self.last_waypoint_flag = True
             except:
                 t = 0
-            if npl.norm(self.trajectoryController.trajectory[-1,0:2]-self.pos) < params.wp_threshold and (t > self.trajectoryController.t[-1] or abs(t - self.trajectoryController.t[-1]) < params.time_threshold): # check if near end pos and end time
-                self.last_waypoint_flag = True
 
             self.wheel_w_cmd, self.wheel_phi_cmd = self.convert2MotorInputs(v_des,w_des)
             self.robot_v_cmd = v_des
