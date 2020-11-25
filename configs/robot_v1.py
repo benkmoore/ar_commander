9dfa967901d692ab617920beee04ad6e4e76ea8e
benkmoore <ben.karl.moore@gmail.com>
2020-11-25 15:45:26 +0000

    L =0.505

--- configs/robot_v1.py
+++ configs/robot_v1.py
@@ -4,7 +4,7 @@ import numpy as np
 N = 4                           # number of wheels
 R1 = np.array([0.375, 0.375])   # position of wheels along arm 1 (m)
 R2 = np.array([0.375, 0.375])   # "" arm 2 (m)
-L = 0.53							# robot arm length from corner (m)
+L = 0.505							# robot arm length from corner (m)
 
 phi_bounds = np.array([-2*np.pi,2*np.pi])  # physical bounds for wheel angle (rads)
 wheel_radius = 0.05             	# m
