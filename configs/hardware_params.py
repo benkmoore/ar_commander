d98b58e5e36fe19a89128d0ab4f9526ae4226e5f
benkmoore <ben.karl.moore@gmail.com>
2020-11-25 12:44:10 +0000

    D=1.0, I=0.01, P=1.2

--- configs/hardware_params.py
+++ configs/hardware_params.py
@@ -14,8 +14,8 @@ max_acceleration = 1.0                     # max acceleration (m/s^2)
 theta_threshold = np.pi/5				   # robot theta threshold at which we transition to next waypoint (rad)
 time_threshold = 1.0
 
-ctrl_tf_state = {'num': np.array([ 0.0170018 , -0.01699168]), 'den': np.array([ 1.        , -1.98299313,  0.98300325])}
-ctrl_tf_state_dot = {'num': np.array([0.00014285]), 'den': np.array([1.        , -0.99985715])}
+ctrl_tf_state = {'num': np.array([ 0.01699776, -0.01699574]), 'den': np.array([ 1.        , -1.98300122,  0.98300325])}
+ctrl_tf_state_dot = {'num': np.array([0.01418416]), 'den': np.array([ 1.        , -0.98581584])}
 
 # filter params obtained from data and testing at: AR1-142
 # Q : covariance on the process noise
