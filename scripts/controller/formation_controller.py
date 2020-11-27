636588219bc515b88310a8977abe40bbc2a58cb0
benkmoore <ben.karl.moore@gmail.com>
2020-11-27 14:50:42 +0000

    0.06

--- scripts/controller/formation_controller.py
+++ scripts/controller/formation_controller.py
@@ -135,6 +135,6 @@ class FormationController():
                 # print(robot_ns, " formation error = ", formation_error)
                 sum_gain += edge_gain
 
-            control = (1.0 / sum_gain) * formation_control * 0.12
+            control = (1.0 / sum_gain) * formation_control * 0.06
 
         return control.flatten()
