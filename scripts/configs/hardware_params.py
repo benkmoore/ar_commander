07f6f2cb5b32690b934d84385c8b736486ad1f9d
benkmoore <ben.karl.moore@gmail.com>
2020-12-09 17:32:50 +0000

    fixed hw params

--- scripts/configs/hardware_params.py
+++ scripts/configs/hardware_params.py
@@ -2,18 +2,29 @@ import numpy as np
 
 CONTROLLER_RATE = 70                       # rate in Hz
 
-max_vel = 3                                # m/s
+startup_time = 6.0                         # s
 
-wp_threshold = 0.05                        # distance threshold at which we transition to next waypoint (m)
+max_vel = 0.5  #0.75                       # m/s
+max_omega = 0.175                          # rad/s
 
-theta_threshold = np.pi/8				   # robot theta threshold at which we transition to next waypoint (rad)
+wp_threshold = 0.4                         # distance threshold at which we transition to next waypoint (m)
 
-trajectoryControllerTF = {'num': np.array([ 0.2, -0.3722704 ,  0.17246762]), 'den': np.array([ 1., -1.97219114,  0.97238837])}
+max_acceleration = 1.0                     # max acceleration (m/s^2)
+
+theta_threshold = np.pi/5				   # robot theta threshold at which we transition to next waypoint (rad)
+time_threshold = 1.0
+
+ctrl_tf_state = {'num': np.array([ 0.00853581, -0.00853378 ]), 'den': np.array([1.        , -1.99146317,  0.9914652])}
+ctrl_tf_state_dot = {'num': np.array([0.01418416]), 'den': np.array([ 1.        , -0.98581584])}
 
 # filter params obtained from data and testing at: AR1-142
 # Q : covariance on the process noise
-positionFilterParams = {"Q": 0.8 * np.eye(2), "Q_d": 0.8 * np.eye(2)}
+positionFilterParams = {"Q": 0.002 * np.eye(2), "Q_d": 0.002 * np.eye(2)}
 
-thetaFilterParams = {"Q": 0.1, "Q_d": 0.1}
+thetaFilterParams = {"Q": 1, "Q_d": 1}
 
 pos_measurement_std = 0.035 # position measurement standard deviation (m)
+
+decawave_ports = ['/dev/ttyACM1','/dev/ttyACM2']
+
+object_offset = {"x": 0.75, "y": 1.2}
