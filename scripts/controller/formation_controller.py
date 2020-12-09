21aa653d54692551218cd631c441f12300ce962c
benkmoore <ben.karl.moore@gmail.com>
2020-12-09 17:29:58 +0000

    import typo

--- scripts/controller/formation_controller.py
+++ scripts/controller/formation_controller.py
@@ -15,9 +15,9 @@ import time
 
 env = rospy.get_param("ENV")
 if env == "sim":
-    import sim_params as params
+    import configs.sim_params as params
 elif env == "hardware":
-    import hardware_params as params
+    import configs.hardware_params as params
 else:
     raise ValueError("Controller ENV: '{}' is not valid. Select from [sim, hardware]".format(env))
 
