41708d04bf0827af2c94772714e06ac98dc851b5
benkmoore <ben.karl.moore@gmail.com>
2020-12-10 10:43:47 +0000

    enable reverse param

--- configs/robot_v1.py
+++ configs/robot_v1.py
@@ -7,5 +7,6 @@ R2 = np.array([0.075, 0.425])   # "" arm 2 (m)
 L = 0.5							# robot arm length from corner (m)
 
 phi_bounds = np.array([-2*np.pi,2*np.pi])  # physical bounds for wheel angle (rads)
+enable_reverse = False                     # turn reverse on drive motors on/off
 wheel_radius = 0.05             	# m
 
